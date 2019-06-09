91d71886faa1a8a136ebd4c0e5ad4d170e6cc5fc
Buck Perley <buck@tierion.com>
2019-06-09 11:58:05 -0500

    initial commit

---
+++ utils/getEssay.js
@@ -0,0 +1,80 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const path = require('path')
+const { parse } = require('url')
+const request = require('request-promise-native')
+const marked = require('marked')
+const TurndownService = require('turndown')
+
+async function getEssay(maxLen = 5, _url) {
+  let url = _url || process.argv[2]
+
+  let page, md
+  try {
+    page = await request(url)
+  } catch (e) {
+    console.log(e.message)
+    process.exit(1)
+  }
+
+  if (path.extname(url).includes('md')) md = page
+  else if (path.extname(url).includes('html')) {
+    const turndownService = new TurndownService()
+    md = turndownService.turndown(page)
+  } else {
+    console.error('Only supports markdown and html pages')
+    process.exit(1)
+  }
+
+  let tokens = marked.lexer(md)
+  let title, subtitle
+  title = tokens.find(token => token.type === 'heading' && token.depth === 1)
+    .text
+  subtitle =
+    tokens[1].type === 'heading' && tokens[1].depth > 1
+      ? tokens[1].text
+      : undefined
+  if (title) tokens = tokens.slice(1)
+  if (subtitle) tokens = tokens.slice(1)
+
+  const text = tokens.reduce((acc, curr) => {
+    switch (curr.type) {
+      case 'space':
+      case 'code':
+      case 'html':
+        break
+      case 'heading':
+        acc.push(curr)
+      case 'paragraph': {
+        const words = curr.text.split(' ')
+        while (words.length) {
+          const text = words.splice(0, maxLen).join(' ')
+          acc.push({ type: 'paragraph', text })
+        }
+      }
+      default:
+        break
+    }
+    return acc
+  }, [])
+  const fileName = title
+    .replace(/[_:\/\[\]\(\)]/g, '')
+    .replace(/\s/g, '-')
+    .toLowerCase()
+  fs.writeFileSync(
+    path.resolve(__dirname, '../src/docs', `${fileName}.json`),
+    JSON.stringify(
+      {
+        title,
+        subtitle,
+        text,
+      },
+      null,
+      2
+    )
+  )
+  return tokens
+}
+
+getEssay()
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const webpack = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+// copy manifest.json to the path: 'public/build'
+// this will allow for the authRequest to see the file at www.example.com/manifest.json
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ManifestAssetPlugin = new CopyWebpackPlugin([
+  { from: "src/assets/manifest.json", to: "manifest.json" }
+]);
+const IconAssetPlugin = new CopyWebpackPlugin([
+  { from: "src/images/icon-192x192.png", to: "icon-192x192.png" }
+]);
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: "./src/index.html",
+  filename: "index.html",
+  inject: "body"
+});
+
+module.exports = {
+  entry: "./src/index.js",
+  target: "web",
+  output: {
+    path: path.resolve("public/build"),
+    filename: "index_bundle.js"
+  },
+  devServer: {
+    historyApiFallback: true,
+    watchOptions: { aggregateTimeout: 300, poll: 1000 },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization"
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+      // { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
+        loader: "file-loader!url-loader"
+      },
+      { test: /\.css$/, loader: "style-loader!css-loader" }
+    ]
+  },
+  plugins: [
+    HtmlWebpackPluginConfig,
+    ManifestAssetPlugin,
+    IconAssetPlugin,
+    new CleanWebpackPlugin()
+  ]
+};
