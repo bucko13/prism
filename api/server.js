94ccaec982ecc6307e1d8f29a071026202cf8d8c
Buck Perley <bucko.perley@gmail.com>
2019-08-29 23:29:52 -0500

    update deps and clean up server code

--- api/server.js
+++ api/server.js
@@ -1,19 +1,20 @@
 /* eslint-disable no-console */
 /* eslint-disable no-console */
 const express = require('express')
-// const helmet = require('helmet')
+const helmet = require('helmet')
 const cookieSession = require('cookie-session')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const webpack = require('webpack')
 const webpackDevMiddleware = require('webpack-dev-middleware')
-
-const config = require('../webpack.config.js')
-const compiler = webpack(config)
-const app = express()
+const path = require('path')
 
 const dotenv = require('dotenv')
-dotenv.config({ path: '../.env' })
+dotenv.config({ path: `${path.resolve(__dirname, '..')}/.env` })
+
+const config = require('../webpack.config.js')(process.env)
+const compiler = webpack(config)
+const app = express()
 
 const metadata = require('./metadata/routes')
 const radiks = require('./radiks/routes')
@@ -27,7 +28,7 @@ app.set('trust proxy', 1)
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
-// app.use(helmet())
+app.use(helmet())
 
 app.use(
   cookieSession({
@@ -51,8 +52,22 @@ app.use(proofs)
 app.use(metadata)
 app.use(
   webpackDevMiddleware(compiler, {
-    publicPath: config.output.publicPath,
+    publicPath: '/',
+    hot: true,
+    host: 'localhost',
   })
 )
+
+app.use('*', function(req, res, next) {
+  var filename = path.join(compiler.outputPath, 'index.html')
+  compiler.outputFileSystem.readFile(filename, function(err, result) {
+    if (err) {
+      return next(err)
+    }
+    res.set('content-type', 'text/html')
+    res.send(result)
+    res.end()
+  })
+})
 
 app.listen(port, () => console.log(`Prism server listening on port ${port}`))
